3b2fec29b80794d80e9b08e85618e2753e6f8b5b
rinsuki <428rinsuki+git@gmail.com>
2020-09-20 04:36:29 +0900

    Timelineちゃんと生成するように

--- Widget/Shared/IdolBirthdayWidget.swift
+++ Widget/Shared/IdolBirthdayWidget.swift
@@ -10,16 +10,16 @@ import SwiftUI
 import Intents
 import Backend
 
-func tomorrow(from today: Date) -> Date? {
-    guard let tomorrowWithHours = Calendar.current.date(byAdding: .day, value: 1, to: today) else {
-        return nil
+func tomorrow(from today: Date, addDay: Int = 1) -> Date {
+    guard let tomorrowWithHours = Calendar.current.date(byAdding: .day, value: addDay, to: today) else {
+        fatalError("why??")
     }
     var tomorrow = Calendar.current.dateComponents(in: .current, from: tomorrowWithHours)
     tomorrow.hour = 0
     tomorrow.minute = 0
     tomorrow.second = 0
     tomorrow.nanosecond = 0
-    return tomorrow.date
+    return tomorrow.date!
 }
 
 struct Provider: IntentTimelineProvider {
@@ -38,55 +38,65 @@ struct Provider: IntentTimelineProvider {
         return .init(date: Date(), content: .placeholder)
     }
     
-    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Entry) -> Void) {
-        IdolManager.request(q: IdolManager.getAllIdolsQuery())
-            .onSuccess { idols in
-                let date = Date()
-                var sortedIdols = idols.sorted {
-                    $0.birthDate.next(current: date) < $1.birthDate.next(current: date)
-                }
-                var todayIdols = [Idol]()
-                var comingSoonIdols = [Idol]()
-                var maxIdolCount = 4
-                var todayIdolsFinished = true
-                while maxIdolCount > 0 {
-                    let idol = sortedIdols.removeFirst()
+    static func buildEntry(from idols: [Idol], configuration: ConfigurationIntent, date: Date = Date()) -> Entry {
+        var sortedIdols = idols.sorted {
+            $0.birthDate.next(current: date) < $1.birthDate.next(current: date)
+        }
+        var todayIdols = [Idol]()
+        var comingSoonIdols = [Idol]()
+        var maxIdolCount = 4
+        var todayIdolsFinished = true
+        while maxIdolCount > 0 {
+            let idol = sortedIdols.removeFirst()
+            maxIdolCount -= 1
+            if idol.birthDate.isBirthDay(current: date, calendar: .current) {
+                todayIdols.append(idol)
+                todayIdolsFinished = false
+            } else {
+                // 今日が誕生日のアイドルがいる場合は"coming soon..."表示が1行取るので
+                // その分を引く必要がある
+                if !todayIdolsFinished {
+                    todayIdolsFinished = true
                     maxIdolCount -= 1
-                    if idol.birthDate.isBirthDay(current: date, calendar: .current) {
-                        todayIdols.append(idol)
-                        todayIdolsFinished = false
-                    } else {
-                        // 今日が誕生日のアイドルがいる場合は"coming soon..."表示が1行取るので
-                        // その分を引く必要がある
-                        if !todayIdolsFinished {
-                            todayIdolsFinished = true
-                            maxIdolCount -= 1
-                        }
-                        comingSoonIdols.append(idol)
-                    }
                 }
-                let entry = Entry(
-                    date: date,
-                    content: .actualData(
-                        todayIdols: todayIdols,
-                        idols: comingSoonIdols,
-                        configuration: configuration
-                    )
-                )
-                completion(entry)
+                comingSoonIdols.append(idol)
+            }
+        }
+        return Entry(
+            date: date,
+            content: .actualData(
+                todayIdols: todayIdols,
+                idols: comingSoonIdols,
+                configuration: configuration
+            )
+        )
+    }
+    
+    static func getIdols(completion: @escaping ([Idol]) -> Void) {
+        IdolManager.request(q: IdolManager.getAllIdolsQuery())
+            .onSuccess {
+                completion($0)
             }
             .onFailure { error in
                 fatalError("\(error)")
             }
     }
     
+    func getSnapshot(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Entry) -> Void) {
+        Self.getIdols { idols in
+            completion(Self.buildEntry(from: idols, configuration: configuration))
+        }
+    }
+    
     func getTimeline(for configuration: ConfigurationIntent, in context: Context, completion: @escaping (Timeline<Entry>) -> Void) {
-        getSnapshot(for: configuration, in: context) { info in
-            if let tomorrow = tomorrow(from: info.date) {
-                completion(.init(entries: [info], policy: .after(tomorrow)))
-            } else {
-                completion(.init(entries: [info], policy: .atEnd))
+        Self.getIdols { idols in
+            var entries = [Entry]()
+            let today = Date()
+            for i in 0..<7 {
+                let date = tomorrow(from: today, addDay: i)
+                entries.append(Self.buildEntry(from: idols, configuration: configuration, date: date))
             }
+            completion(.init(entries: entries, policy: .after(tomorrow(from: today))))
         }
     }
 }
